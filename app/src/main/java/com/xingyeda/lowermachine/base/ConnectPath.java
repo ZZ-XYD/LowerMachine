95c3ea2ed10170c070819ed60773ddd8af8f9bec
xingyeda <zz_xingyeda@163.com>
2017-11-08 17:30:33 +0800

    HTTP请求统一接口类

--- app/src/main/java/com/xingyeda/lowermachine/base/ConnectPath.java
+++ app/src/main/java/com/xingyeda/lowermachine/base/ConnectPath.java
@@ -5,12 +5,12 @@ package com.xingyeda.lowermachine.base;
  */
 public class ConnectPath {
 
-//    public static String HOST = "http://service.xyd999.com:8080/";
-    public static final String HOST = "http://192.168.10.200:8080/";
-//    public static final String HOST = "http://192.168.10.250:8080/";
+    //    public static String HOST = "http://service.xyd999.com:8080/";
+//    public static final String HOST = "http://192.168.10.200:8080/";
+    public static final String HOST = "http://192.168.10.250:8080/";
 
-    public static String HOST_PATH = HOST + "xydServer/servlet/";
+    public static final String HOST_PATH = HOST + "xydServer/servlet/";
 
     //传送MAC地址添加设备
-    public static String ADDSHEBEIFORAPP = HOST_PATH + "addShebeiForApp";
+    public static final String ADDSHEBEIFORAPP = HOST_PATH + "addShebeiForApp";
 }
