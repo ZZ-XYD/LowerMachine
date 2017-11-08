c549367d411f1be0ed6f5d8a511311b3de8dbf7c
xingyeda <zz_xingyeda@163.com>
2017-11-08 17:29:03 +0800

    设备SN码实体类

---
+++ app/src/main/java/com/xingyeda/lowermachine/bean/SNCode.java
@@ -0,0 +1,35 @@
+package com.xingyeda.lowermachine.bean;
+
+/*
+设备SN码
+ */
+public class SNCode {
+
+    private String obj;
+    private String status;
+    private String msg;
+
+    public String getObj() {
+        return obj;
+    }
+
+    public void setObj(String obj) {
+        this.obj = obj;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
