b0d96282ee53c2c42d308d6b19cd5af63a2c5c03
xingyeda <zz_xingyeda@163.com>
2017-11-08 16:20:13 +0800

    HTTP接口类

---
+++ app/src/main/java/com/xingyeda/lowermachine/base/InterfacePath.java
@@ -0,0 +1,16 @@
+package com.xingyeda.lowermachine.base;
+
+/*
+接口类
+ */
+public class InterfacePath {
+
+    public static String HOST = "http://service.xyd999.com:8080/";
+//    public static final String HOST = "http://192.168.10.200:8080/";
+//    public static final String HOST = "http://192.168.10.250:8080/";
+
+    public static String HOST_PATH = HOST + "xydServer/servlet/";
+
+    //传送MAC地址添加设备
+    public static String ADDSHEBEIFORAPP = HOST_PATH + "addShebeiForApp";
+}
