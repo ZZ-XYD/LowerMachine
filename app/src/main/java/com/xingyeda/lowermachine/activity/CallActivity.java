b4c1d8b4048e47cbba56c31f66525fff7408984e
LDL <377438996@qq.com>
2017-11-10 17:21:16 +0800

    呼叫界面

---
+++ app/src/main/java/com/xingyeda/lowermachine/activity/CallActivity.java
@@ -0,0 +1,350 @@
+package com.xingyeda.lowermachine.activity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.xingyeda.lowermachine.R;
+import com.xingyeda.lowermachine.base.BaseActivity;
+import com.xingyeda.lowermachine.base.ConnectPath;
+import com.xingyeda.lowermachine.business.MainBusiness;
+import com.xingyeda.lowermachine.http.ConciseCallbackHandler;
+import com.xingyeda.lowermachine.http.ConciseStringCallback;
+import com.xingyeda.lowermachine.http.OkHttp;
+import com.xingyeda.lowermachine.utils.BaseUtils;
+import com.xingyeda.lowermachine.utils.HttpUtils;
+import com.xingyeda.lowermachine.utils.SharedPreUtil;
+import com.xingyeda.lowermachine.view.layout.PercentLinearLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.agora.rtc.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class CallActivity extends BaseActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int PERMISSION_REQ_ID_RECORD_AUDIO = 22;
+    private static final int PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1;
+    @BindView(R.id.test)
+    PercentLinearLayout test;
+
+    private RtcEngine mRtcEngine;//  教程步骤 1
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() { // 教程步骤1  回调
+        @Override
+        public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) { //  教程步骤 5  远端视频接收解码回调
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) { //  教程步骤 7 其他用户离开当前频道回调
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    onRemoteUserLeft();
+                }
+            });
+        }
+
+        @Override
+        public void onUserMuteVideo(final int uid, final boolean muted) { //  教程步骤 10  其他用户已停发/已重发视频流回调
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+//                    onRemoteUserVideoMuted(uid, muted);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+        ButterKnife.bind(this);
+
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO) && checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)) {
+            initAgoraEngineAndJoinChannel();
+        }
+    }
+
+    //初始化声网引擎和加入频道
+    private void initAgoraEngineAndJoinChannel() {
+        initializeAgoraEngine();     //  教程步骤 1
+        setupVideoProfile();         //  教程步骤 2
+        setupLocalVideo();           //  教程步骤 3
+        joinChannel();               //  教程步骤 4
+    }
+
+    //自我检查权限
+    public boolean checkSelfPermission(String permission, int requestCode) {
+        Log.i(LOG_TAG, "自我检查权限 " + permission + " " + requestCode);
+        if (ContextCompat.checkSelfPermission(this,
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission},
+                    requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        Log.i(LOG_TAG, "请求权限的结果 " + grantResults[0] + " " + requestCode);
+
+        switch (requestCode) {
+            case PERMISSION_REQ_ID_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA);
+                } else {
+                    showLongToast("没有许可 " + Manifest.permission.RECORD_AUDIO);
+                    finish();
+                }
+                break;
+            }
+            case PERMISSION_REQ_ID_CAMERA: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initAgoraEngineAndJoinChannel();
+                } else {
+                    showLongToast("没有许可 " + Manifest.permission.CAMERA);
+                    finish();
+                }
+                break;
+            }
+        }
+    }
+
+    public final void showLongToast(final String msg) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        leaveChannel();
+        RtcEngine.destroy();//销毁引擎实例
+        mRtcEngine = null;
+    }
+
+    // 教程步骤 10   本地视频开关
+    public void onLocalVideoMuteClicked(View view) {
+        ImageView iv = (ImageView) view;
+        if (iv.isSelected()) {
+            iv.setSelected(false);
+            iv.clearColorFilter();
+        } else {
+            iv.setSelected(true);
+            iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        }
+
+        mRtcEngine.muteLocalVideoStream(iv.isSelected());
+
+        FrameLayout container = (FrameLayout) findViewById(R.id.local_video_view_container);
+        SurfaceView surfaceView = (SurfaceView) container.getChildAt(0);
+        surfaceView.setZOrderMediaOverlay(!iv.isSelected());
+        surfaceView.setVisibility(iv.isSelected() ? View.GONE : View.VISIBLE);
+    }
+
+    // 教程步骤 9  本地音频静音（声控制）
+    public void onLocalAudioMuteClicked(View view) {
+        ImageView iv = (ImageView) view;
+        if (iv.isSelected()) {
+            iv.setSelected(false);
+            iv.clearColorFilter();
+        } else {
+            iv.setSelected(true);
+            iv.setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        }
+
+        mRtcEngine.muteLocalAudioStream(iv.isSelected());
+    }
+
+    //  教程步骤 8 摄像头切换
+    public void onSwitchCameraClicked(View view) {
+        mRtcEngine.switchCamera();
+    }
+
+    //  教程步骤 6  挂断
+    public void onEncCallClicked(View view) {
+        finish();
+    }
+
+    //  教程步骤 1
+    private void initializeAgoraEngine() {
+        try {
+            /**
+             * 创建 RtcEngine 对象。
+             * 上下文
+             * appid
+             * 加入回调
+             */
+            mRtcEngine = RtcEngine.create(getBaseContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+
+            throw new RuntimeException("需要检查rtc sdk init致命错误\n" + Log.getStackTraceString(e));
+        }
+    }
+
+    //  教程步骤 2
+    private void setupVideoProfile() {
+        mRtcEngine.enableVideo();//打开视频模式
+        /**
+         * 设置本地视频属性
+         * 视频属性
+         * 是否交换宽高
+         */
+        mRtcEngine.setVideoProfile(Constants.VIDEO_PROFILE_360P, false);
+    }
+
+    //  教程步骤 3
+    private void setupLocalVideo() {
+        FrameLayout container = (FrameLayout) findViewById(R.id.local_video_view_container);
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());//创建渲染视图 ----上下文
+        surfaceView.setZOrderMediaOverlay(true);
+        container.addView(surfaceView);//将渲染视图添加进去
+        /**
+         * 设置本地视频显示属性
+         * VideoCanvas
+         *        视频显示视窗
+         *        视频显示模式  RENDER_MODE_HIDDEN (1): 如果视频尺寸与显示视窗尺寸不一致，则视频流会按照显示视窗的比例进行周边裁剪或图像拉伸后填满视窗。
+         RENDER_MODE_FIT(2): 如果视频尺寸与显示视窗尺寸不一致，在保持长宽比的前提下，将视频进行缩放后填满视窗。
+         *        本地用户 ID，与 joinChannel 方法中的 uid 保持一致
+         */
+        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_ADAPTIVE, 0));
+    }
+
+    //  教程步骤 4
+    private void joinChannel() {
+        /**
+         * 房间钥匙：可以为空
+         * 房间名称：也就是通道
+         * 房间信息：也就是通道信息
+         * 用户id ：会在onJoinChannelSuccess返回
+         */
+        mRtcEngine.joinChannel(null, "demoChannel1", "Extra Optional Data", 0); // 如果你不指定uid,我们会为您生成的uid
+
+    }
+
+    //    //  教程步骤 5  设置远端视频显示属性 （连接）
+    private void setupRemoteVideo(int uid) {
+//        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+//
+//        if (container.getChildCount() >= 1) {
+//            return;
+//        }
+//
+//        SurfaceView surfaceView = RtcEngine.CreateRendererView(getBaseContext());
+//        container.addView(surfaceView);
+//        /**
+//         * 设置本地视频显示属性
+//         * VideoCanvas
+//         *        视频显示视窗
+//         *        视频显示模式
+//         *        本地用户 ID，与 joinChannel 方法中的 uid 保持一致
+//         */
+//        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_ADAPTIVE, uid));
+//
+//        surfaceView.setTag(uid); // 马克的目的
+//        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // 可选的界面
+//        tipMsg.setVisibility(View.GONE);
+    }
+
+    //  教程步骤 6  离开频道
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();//离开频道
+    }
+
+    //  教程步骤 7  远程用户离开  （挂断）
+    private void onRemoteUserLeft() {
+//        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+//        container.removeAllViews();
+//
+//        View tipMsg = findViewById(R.id.quick_tips_when_use_agora_sdk); // 可选的界面
+//        tipMsg.setVisibility(View.VISIBLE);
+    }
+
+    //  教程步骤 10   远程用户已停发/已重发视频流
+    private void onRemoteUserVideoMuted(int uid, boolean muted) {
+//        FrameLayout container = (FrameLayout) findViewById(R.id.remote_video_view_container);
+//
+//        SurfaceView surfaceView = (SurfaceView) container.getChildAt(0);
+//
+//        Object tag = surfaceView.getTag();
+//        if (tag != null && (Integer) tag == uid) {
+//            surfaceView.setVisibility(muted ? View.GONE : View.VISIBLE);
+//        }
+    }
+
+
+    private void callOut(String callinfo) {
+        Map<String, String> params = new HashMap<>();
+        params.put("randomCode", getRandom());
+        params.put("callinfo", callinfo);
+        params.put("eid", MainBusiness.getMacAddress(mContext));
+        params.put("block", "00");
+        params.put("isxiaoqu", SharedPreUtil.getString(mContext, "isxiaoqu"));
+        OkHttp.get(ConnectPath.CALLUSER_PATH(mContext), params,new ConciseStringCallback(mContext, new ConciseCallbackHandler<String>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+            }
+        }));
+
+
+    }
+
+    private String getRandom() {
+        String strRand = "";
+        for (int i = 0; i < 8; i++) {
+            strRand += String.valueOf((int) (Math.random() * 10));
+        }
+        return strRand;
+    }
+
+    @OnClick(R.id.test)
+    public void onViewClicked() {
+        callOut("8888");
+    }
+}
