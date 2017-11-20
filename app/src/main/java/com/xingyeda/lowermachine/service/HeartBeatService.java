4305dddb821e8a55a26abfcd85b4d9512ea647ad
xingyeda <zz_xingyeda@163.com>
2017-11-20 10:37:34 +0800

    添加广播处理

--- app/src/main/java/com/xingyeda/lowermachine/service/HeartBeatService.java
+++ app/src/main/java/com/xingyeda/lowermachine/service/HeartBeatService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Handler;
 import android.os.IBinder;
 
 import com.hurray.plugins.rkctrl;
@@ -11,6 +12,7 @@ import com.xingyeda.lowermachine.R;
 import com.xingyeda.lowermachine.base.Commond;
 import com.xingyeda.lowermachine.bean.Message;
 import com.xingyeda.lowermachine.business.MainBusiness;
+import com.xingyeda.lowermachine.utils.BaseUtils;
 import com.xingyeda.lowermachine.utils.JsonUtils;
 
 import java.io.IOException;
@@ -27,6 +29,7 @@ public class HeartBeatService extends Service {
     private OutputStream out = null;
     private rkctrl m_rkctrl = new rkctrl();
     private SoundPool mSoundPool;
+    private Handler mHandler = new Handler();
 
     public HeartBeatService() {
     }
@@ -98,9 +101,12 @@ public class HeartBeatService extends Service {
         if (mSocket == null) {
             try {
                 mSocket = new Socket("192.168.10.200", 5888);
-                Intent intent = new Intent();
-                intent.setAction("HeartBeatSocketConnected");
-                HeartBeatService.this.sendBroadcast(intent);
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        BaseUtils.showShortToast(getApplicationContext(), "SocketConnected");
+                    }
+                });
                 if (out == null) {
                     out = mSocket.getOutputStream();
                 }
@@ -169,7 +175,12 @@ public class HeartBeatService extends Service {
                         intent.setAction("HeartBeatService.MOBILE_RECEIVE");
                         HeartBeatService.this.sendBroadcast(intent);
                     } else if (str.equals(Commond.PC_RESTART)) {//重启
-
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_REBOOT);
+                        intent.putExtra("nowait", 1);
+                        intent.putExtra("interval", 1);
+                        intent.putExtra("window", 0);
+                        sendBroadcast(intent);
                     } else if (str.equals(Commond.NO_ANSWER)) {//无应答
                         mSoundPool.play(2, 1, 1, 0, 0, 1);
                     } else if (str.equals(Commond.BUSY)) {//用户通话中
