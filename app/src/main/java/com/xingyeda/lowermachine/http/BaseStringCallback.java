668e7f35b7052dab8d82bde473c3c1aae6e3e7f4
LDL <377438996@qq.com>
2017-11-21 14:04:39 +0800

    界面设置

--- app/src/main/java/com/xingyeda/lowermachine/http/BaseStringCallback.java
+++ app/src/main/java/com/xingyeda/lowermachine/http/BaseStringCallback.java
@@ -23,7 +23,7 @@ public class BaseStringCallback extends StringCallback {
     }
     @Override
     public void onError(Call call, Exception e,int id) {
-        BaseUtils.showShortToast(mContext, "连接超时");
+//        BaseUtils.showShortToast(mContext, "连接超时");
 //		MyLog.i("连接超时");
 		mCallbackHandler.onFailure();
 //        mHandler.sendEmptyMessage(TIMEOUT);
@@ -37,7 +37,7 @@ public class BaseStringCallback extends StringCallback {
 	    JSONObject jobj= new JSONObject(response);
 	    if (!jobj.get("status").equals("200")) {
 		if (jobj.has("msg")) {
-			BaseUtils.showShortToast(mContext, jobj.getString("msg"));
+//			BaseUtils.showShortToast(mContext, jobj.getString("msg"));
 		}
 			mCallbackHandler.parameterError(jobj);
 		return;
