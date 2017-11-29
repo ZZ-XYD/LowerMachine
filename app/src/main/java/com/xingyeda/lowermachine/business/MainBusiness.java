17d63c2747bb3c57a7398fea861b4e4ff3d0ed2a
xingyeda <zz_xingyeda@163.com>
2017-11-29 16:35:04 +0800

    添加设备更新代码

--- app/src/main/java/com/xingyeda/lowermachine/business/MainBusiness.java
+++ app/src/main/java/com/xingyeda/lowermachine/business/MainBusiness.java
@@ -4,12 +4,21 @@ import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.xingyeda.lowermachine.base.ConnectPath;
 import com.xingyeda.lowermachine.bean.SNCode;
+import com.xingyeda.lowermachine.utils.BaseUtils;
 import com.xingyeda.lowermachine.utils.HttpUtils;
 import com.xingyeda.lowermachine.utils.JsonUtils;
 import com.xingyeda.lowermachine.utils.SharedPreferencesUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +34,7 @@ public class MainBusiness {
     public static void getSN(final Context context) {
         Map map = new HashMap();
         map.put("mac", getMacAddress(context));
-        HttpUtils.doPost(ConnectPath.getPath(context,ConnectPath.ADDSHEBEIFORAPP), map, new Callback() {
+        HttpUtils.doPost(ConnectPath.getPath(context, ConnectPath.ADDSHEBEIFORAPP), map, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
 
@@ -46,8 +55,86 @@ public class MainBusiness {
     public static String getMacAddress(Context context) {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        return wifiInfo.getMacAddress().replaceAll(":","");
+        return wifiInfo.getMacAddress().replaceAll(":", "");
     }
 
+    /*
+    获取版本
+     */
 
+    public static void getVersion(final Context context) {
+        HttpUtils.doGet(ConnectPath.getPath(context, ConnectPath.GETSERVERVERSION), new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String serverURL = "";
+                String fileName = "";
+                String versionNumber = "";
+                String downPath = "";
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    if (jsonObject.has("obj")) {
+                        String obj = jsonObject.getString("obj");
+                        JSONObject jsonObj = new JSONObject(obj);
+                        if (jsonObj.has("ServerURL")) {
+                            serverURL = jsonObj.getString("ServerURL");
+                        }
+                        if (jsonObj.has("files")) {
+                            JSONArray jsonArray = (JSONArray) jsonObj.get("files");
+                            JSONObject filesArray = (JSONObject) jsonArray.get(0);
+                            if (filesArray.has("fileName")) {
+                                fileName = filesArray.getString("fileName");
+                            }
+                        }
+                        downPath = serverURL + fileName;
+                        if (jsonObj.has("versionNumber")) {
+                            versionNumber = jsonObj.getString("versionNumber");
+                        }
+                        if (Integer.valueOf(BaseUtils.getVersionCode(context)) < Integer.valueOf(versionNumber)) {
+                            downloadUpdate(downPath, context);
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /*
+    从服务器获取APK
+    */
+    public static void downloadUpdate(String downPath, final Context context) {
+        final String apkPath = BaseUtils.initFile(context) + "/" + "LowerMachine.apk";
+        com.lidroid.xutils.HttpUtils mHttpUtils = new com.lidroid.xutils.HttpUtils();
+        mHttpUtils.download(downPath, apkPath, true, true, new RequestCallBack<File>() {
+            @Override
+            public void onSuccess(ResponseInfo<File> responseInfo) {
+                String[] command = {"chmod", "777", apkPath};
+                ProcessBuilder builder = new ProcessBuilder(command);
+                try {
+                    builder.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                final File file_apk = (File) responseInfo.result;
+
+                try {
+                    Thread.sleep(1000 * 3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                BaseUtils.installApk(file_apk, context);
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+
+            }
+        });
+    }
 }
